196e9214109facef708f51c101d963dc04a2da4a
mujeebreh200 <mujeebreh200@gmail.com>
2026-01-05 00:48:37 +0100

    intial Pack and libaray

---
+++ intial package and libaray.R
@@ -0,0 +1,8 @@
+# 1. Loading Packages
+# Core libraries for data science and web application
+library(shiny)        # Application framework
+library(tidyverse)    # Data manipulation (dplyr, ggplot2, tidyr, pipes)
+library(plotly)       # Interactive visualizations
+library(bslib)        # Modern UI & Floating elements
+library(DT)           # Interactive Tables
+library(janitor)      # Cleaning messy header names
